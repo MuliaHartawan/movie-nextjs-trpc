e03fc9b7637706887abc4d1ff53376c8effa708d
Maulana Sodiqin <sodiqincahyana1@gmail.com>
2024-07-10 13:56:15 +0700

    feat: add rbac schema

--- libs/drizzle/schema.ts
+++ libs/drizzle/schema.ts
@@ -1,25 +1,31 @@
-import exp from "constants";
 import { timestamp, pgTable, text, uuid } from "drizzle-orm/pg-core";
 
 const defaultImage =
   "https://w7.pngwing.com/pngs/205/731/png-transparent-default-avatar-thumbnail.png";
 
-export const users = pgTable("app_user", {
+export const roles = pgTable("app_role", {
   id: uuid("id").defaultRandom().primaryKey(),
-  otp: text("otp"),
-  fullname: text("fullname"),
-  address: text("address"),
-  password: text("password"),
-  email: text("email").notNull().unique(),
-  emailVerified: timestamp("emailVerified", { mode: "date" }),
-  image: text("image").default(defaultImage),
+  name: text("name").notNull(),
+  permissions: text("permissions").notNull().array(),
   createdAt: timestamp("createdAt", { mode: "date" }).defaultNow(),
   updatedAt: timestamp("updatedAt", { mode: "date" }).defaultNow(),
 });
 
+export const users = pgTable("app_user", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  otp: text("otp"),
+  email: text("email").notNull().unique(),
+  image: text("image").default(defaultImage),
+  roleId: uuid("roleId").references(() => roles.id, { onDelete: "cascade" }),
+  address: text("address"),
+  fullname: text("fullname"),
+  password: text("password"),
+  emailVerified: timestamp("emailVerified", { mode: "date" }),
+  createdAt: timestamp("createdAt", { mode: "date" }).defaultNow(),
+  updatedAt: timestamp("updatedAt", { mode: "date" }).defaultNow(),
+});
 
 const now = new Date();
-// Default expiry date is 1 day from now
 const defaultExpiryDate = new Date(now.setDate(now.getDate() + 1));
 
 export const snacks = pgTable("snacks", {
