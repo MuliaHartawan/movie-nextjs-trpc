82b1fbb5c36f805e8feeee67ade92599dffceb0c
avlach19054 <muhammadzakia.19054@mhs.unesa.ac.id>
2024-07-10 10:28:46 +0700

    feat: generate schema `snacks`

--- libs/drizzle/schema.ts
+++ libs/drizzle/schema.ts
@@ -1,3 +1,4 @@
+import exp from "constants";
 import { timestamp, pgTable, text, uuid } from "drizzle-orm/pg-core";
 
 const defaultImage =
@@ -12,6 +13,20 @@ export const users = pgTable("app_user", {
   email: text("email").notNull().unique(),
   emailVerified: timestamp("emailVerified", { mode: "date" }),
   image: text("image").default(defaultImage),
+  createdAt: timestamp("createdAt", { mode: "date" }).defaultNow(),
+  updatedAt: timestamp("updatedAt", { mode: "date" }).defaultNow(),
+});
+
+
+const now = new Date();
+// Default expiry date is 1 day from now
+const defaultExpiryDate = new Date(now.setDate(now.getDate() + 1));
+
+export const snacks = pgTable("snacks", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: text("name").notNull(),
+  price: text("price").notNull(),
+  expiryDate: timestamp("expiryDate", { mode: "date" }).default(defaultExpiryDate),
   createdAt: timestamp("createdAt", { mode: "date" }).defaultNow(),
   updatedAt: timestamp("updatedAt", { mode: "date" }).defaultNow(),
 });
