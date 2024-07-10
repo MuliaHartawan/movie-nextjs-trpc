2e7ac0bcba998e33b603ed0794ab65d14810a3fc
avlach19054 <muhammadzakia.19054@mhs.unesa.ac.id>
2024-07-10 14:58:43 +0700

    improve: define role for seeding users

--- libs/drizzle/seeders/dummy-user.ts
+++ libs/drizzle/seeders/dummy-user.ts
@@ -1,17 +1,18 @@
-import * as uuid from "uuid";
-import { User } from "@/app/(authenticated)/dashboard/(list-user)/actions/get-users";
 import { hashPassword } from "../../auth/password";
-import { defaultImage, users } from "../schema";
+import { defaultImage, roles, users } from "../schema";
 import { PostgresJsDatabase } from "drizzle-orm/postgres-js";
-
-const { v4: uuidv4 } = uuid;
+import { ROLE_DUMMY } from "@/common/enums/role-dummy.enum";
+import { User } from "../schemas/user.schema";
 
 export const seedUsers = async (db: PostgresJsDatabase<Record<string, never>>) => {
     console.log("Seeding users...");
     const dummyUsers: User[] = [];
+    
+    const existingRoles = await db.select().from(roles);
+
+    // Create 1000 dummy users
     for (let i = 0; i < 1000; i++) {
         const dummyUser: User = {
-            id: uuidv4(),
             otp: null,
             fullname: `John Doe ${i+1}`,
             email: `johndoe@${i+1}email.com`,
@@ -19,9 +20,20 @@ export const seedUsers = async (db: PostgresJsDatabase<Record<string, never>>) =
             password: await hashPassword(`password${i+1}`),
             image: defaultImage,
             emailVerified: new Date(),
-            createdAt: new Date(),
-            updatedAt: new Date(),
         };
+
+        // Define the user's role with conditions
+        if (i < 200) { // 0 - 199
+            dummyUser.roleId = existingRoles.find(role => role.name === ROLE_DUMMY.ADMIN)?.id;
+        } else if (i >= 200 && i < 400) { // 200 - 399
+            dummyUser.roleId = existingRoles.find(role => role.name === ROLE_DUMMY.STAFF)?.id;
+        } else if (i >= 400 && i < 600) { // 400 - 599
+            dummyUser.roleId = existingRoles.find(role => role.name === ROLE_DUMMY.ADMIN_SNACK)?.id;
+        } else if (i >= 600 && i < 800) { // 600 - 799
+            dummyUser.roleId = existingRoles.find(role => role.name === ROLE_DUMMY.ADMIN_USER)?.id;
+        } else { // 800 - 999
+            dummyUser.roleId = existingRoles.find(role => role.name === ROLE_DUMMY.ADMIN_ROLE)?.id;
+        }
 
         dummyUsers.push(dummyUser);
     }
