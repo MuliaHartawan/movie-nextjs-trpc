4bc776f4c43430d5d54179bf8aa65ce8c134b23e
avlach19054 <muhammadzakia.19054@mhs.unesa.ac.id>
2024-07-11 11:36:57 +0700

    feat: init page for dashboard role

---
+++ app/(authenticated)/dashboard/(list-role)/roles/page.tsx
@@ -0,0 +1,20 @@
+"use server";
+import type { ReactElement } from "react";
+import { DashboardRolesModule } from "../_modules";
+import { getRoles } from "../actions/get-roles";
+
+const DashboardRoles = async ({
+  searchParams,
+}: {
+  searchParams?: { [key: string]: string | string[] | undefined };
+}): Promise<ReactElement> => {
+  const data = await getRoles({
+    page: Number(searchParams?.page || 1),
+    perPage: Number(searchParams?.perPage || 10),
+    search: String(searchParams?.search || ""),
+  });
+  //console.log(data);
+  return <DashboardRolesModule data={data} />;
+};
+
+export default DashboardRoles;
