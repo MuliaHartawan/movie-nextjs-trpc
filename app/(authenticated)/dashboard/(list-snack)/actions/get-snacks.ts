e373c46a03071cfa3a2364aabe0eaa7d5f0136b6
avlach19054 <muhammadzakia.19054@mhs.unesa.ac.id>
2024-07-11 09:33:55 +0700

    improve: add try catch for handling error

--- app/(authenticated)/dashboard/(list-snack)/actions/get-snacks.ts
+++ app/(authenticated)/dashboard/(list-snack)/actions/get-snacks.ts
@@ -1,11 +1,12 @@
 "use server";
 import { db } from "@/libs/drizzle/connection";
-import { snacks, users } from "@/libs/drizzle/schema";
+import { snacks } from "@/libs/drizzle/schema";
 import { TMetaItem, TMetaResponse } from "@/types/meta";
 import { createSelectSchema } from "drizzle-zod";
 import { z } from "zod";
 import { asc, sql } from "drizzle-orm";
 import { calculateTotalPages, metaResponsePrefix } from "@/utils";
+import { ErrorMapper } from "@/common/types/error-500-mapper.types";
 
 const selectSnackSchema = createSelectSchema(snacks);
 
@@ -17,41 +18,54 @@ export const getSnacks = async (meta: TMetaItem): Promise<TMetaResponse<Snack[]>
   const offset = (page - 1) * perPage;
   const search = meta?.search;
 
-  const query = db
-    .select()
-    .from(snacks);
+  try {
+    const query = db
+      .select()
+      .from(snacks);
 
-  if (search) {
-    query.where(sql`lower(${snacks.name}) like lower('%' || ${search} || '%')`);
+    if (search) {
+      query.where(sql`lower(${snacks.name}) like lower('%' || ${search} || '%')`);
+    }
+
+    const data = await query
+      .limit(perPage)
+      .offset(offset)
+      .orderBy(snacks.createdAt, asc(snacks.createdAt));
+
+    const count = await db
+      .select({ id: snacks.id })
+      .from(snacks)
+      .then((res) => res.length);
+
+    const totalPage = calculateTotalPages(count, perPage);
+    const nextPage = page < totalPage ? page + 1 : null;
+    const prevPage = page > 1 ? page - 1 : null;
+
+    const metaPrefix: TMetaResponse<Snack[]> = {
+      data,
+      meta: {
+        code: 200,
+        status: "success",
+        message: "Berhasil menampilkan snack",
+        page,
+        perPage,
+        totalPage,
+        nextPage,
+        prevPage,
+      },
+    };
+
+    return metaResponsePrefix(metaPrefix);
+  } catch (error) {
+    const errorResponse: ErrorMapper<Snack[]> = {
+      meta: {
+        code: 500,
+        status: "error",
+        message: error as string,
+      },
+      data: [],
+    };
+
+    return errorResponse;
   }
-
-  const data = await query
-    .limit(perPage)
-    .offset(offset)
-    .orderBy(snacks.createdAt, asc(snacks.createdAt));
-
-  const count = await db
-    .select({ id: snacks.id })
-    .from(snacks)
-    .then((res) => res.length);
-
-  const totalPage = calculateTotalPages(count, perPage);
-  const nextPage = page < totalPage ? page + 1 : null;
-  const prevPage = page > 1 ? page - 1 : null;
-
-  const metaPrefix: TMetaResponse<Snack[]> = {
-    data,
-    meta: {
-      code: 200,
-      status: "success",
-      message: "Berhasil menampilkan snack",
-      page,
-      perPage,
-      totalPage,
-      nextPage,
-      prevPage,
-    },
-  };
-
-  return metaResponsePrefix(metaPrefix);
 };
