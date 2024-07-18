c59a489b54e5478e3eb3f31772e4fe8ed19075cf
Daniel Prastha <danielprastha4@gmail.com>
2024-07-18 10:19:49 +0700

    feat: use node postgres

--- libs/drizzle/seeders/dummy-role.ts
+++ libs/drizzle/seeders/dummy-role.ts
@@ -1,85 +1,84 @@
-import { PostgresJsDatabase } from "drizzle-orm/postgres-js";
-
+import { NodePgDatabase } from "drizzle-orm/node-postgres";
 import { Role } from "../schemas/role.schema";
 import { PERMISSIONS } from "@/common/enums/permissions.enum";
 import { roles } from "../schema";
 import { ROLE_DUMMY } from "@/common/enums/role-dummy.enum";
 
-export const seedRoles = async (db: PostgresJsDatabase<Record<string, never>>) => {
-    console.log("Seeding roles...");
-    const dummyRoles: Role[] = [
-        {
-            name: ROLE_DUMMY.ADMIN,
-            permissions: [
-                PERMISSIONS.DASHBOARD,
-                PERMISSIONS.SNACK_CREATE,
-                PERMISSIONS.SNACK_READ,
-                PERMISSIONS.SNACK_UPDATE,
-                PERMISSIONS.SNACK_DELETE,
-                PERMISSIONS.SNACK_DETAIL,
-                PERMISSIONS.USER_CREATE,
-                PERMISSIONS.USER_READ,
-                PERMISSIONS.USER_UPDATE,
-                PERMISSIONS.USER_DELETE,
-                PERMISSIONS.USER_DETAIL,
-                PERMISSIONS.ROLE_CREATE,
-                PERMISSIONS.ROLE_UPDATE,
-                PERMISSIONS.ROLE_DELETE,
-                PERMISSIONS.ROLE_DETAIL,
-                PERMISSIONS.ROLE_READ,
-            ],
-        },
-        {
-            name: ROLE_DUMMY.STAFF,
-            permissions: [
-                PERMISSIONS.DASHBOARD,
-                PERMISSIONS.SNACK_READ,
-                PERMISSIONS.SNACK_DETAIL,
-                PERMISSIONS.USER_READ,
-                PERMISSIONS.USER_DETAIL,
-                PERMISSIONS.ROLE_READ,
-                PERMISSIONS.ROLE_DETAIL,
-            ],
-        },
-        {
-            name: ROLE_DUMMY.ADMIN_SNACK,
-            permissions: [
-                PERMISSIONS.DASHBOARD,
-                PERMISSIONS.SNACK_READ,
-                PERMISSIONS.SNACK_DETAIL,
-                PERMISSIONS.SNACK_CREATE,
-                PERMISSIONS.SNACK_UPDATE,
-                PERMISSIONS.SNACK_DELETE,
-            ],
-        },
-        {
-            name: ROLE_DUMMY.ADMIN_USER,
-            permissions: [
-                PERMISSIONS.DASHBOARD,
-                PERMISSIONS.USER_READ,
-                PERMISSIONS.USER_DETAIL,
-                PERMISSIONS.USER_CREATE,
-                PERMISSIONS.USER_UPDATE,
-                PERMISSIONS.USER_DELETE,
-            ],
-        },
-        {
-            name: ROLE_DUMMY.ADMIN_ROLE,
-            permissions: [
-                PERMISSIONS.DASHBOARD,
-                PERMISSIONS.ROLE_READ,
-                PERMISSIONS.ROLE_DETAIL,
-                PERMISSIONS.ROLE_CREATE,
-                PERMISSIONS.ROLE_UPDATE,
-                PERMISSIONS.ROLE_DELETE,
-            ],
-        },
-    ];
+export const seedRoles = async (db: NodePgDatabase<Record<string, never>>) => {
+  console.log("Seeding roles...");
+  const dummyRoles: Role[] = [
+    {
+      name: ROLE_DUMMY.ADMIN,
+      permissions: [
+        PERMISSIONS.DASHBOARD,
+        PERMISSIONS.SNACK_CREATE,
+        PERMISSIONS.SNACK_READ,
+        PERMISSIONS.SNACK_UPDATE,
+        PERMISSIONS.SNACK_DELETE,
+        PERMISSIONS.SNACK_DETAIL,
+        PERMISSIONS.USER_CREATE,
+        PERMISSIONS.USER_READ,
+        PERMISSIONS.USER_UPDATE,
+        PERMISSIONS.USER_DELETE,
+        PERMISSIONS.USER_DETAIL,
+        PERMISSIONS.ROLE_CREATE,
+        PERMISSIONS.ROLE_UPDATE,
+        PERMISSIONS.ROLE_DELETE,
+        PERMISSIONS.ROLE_DETAIL,
+        PERMISSIONS.ROLE_READ,
+      ],
+    },
+    {
+      name: ROLE_DUMMY.STAFF,
+      permissions: [
+        PERMISSIONS.DASHBOARD,
+        PERMISSIONS.SNACK_READ,
+        PERMISSIONS.SNACK_DETAIL,
+        PERMISSIONS.USER_READ,
+        PERMISSIONS.USER_DETAIL,
+        PERMISSIONS.ROLE_READ,
+        PERMISSIONS.ROLE_DETAIL,
+      ],
+    },
+    {
+      name: ROLE_DUMMY.ADMIN_SNACK,
+      permissions: [
+        PERMISSIONS.DASHBOARD,
+        PERMISSIONS.SNACK_READ,
+        PERMISSIONS.SNACK_DETAIL,
+        PERMISSIONS.SNACK_CREATE,
+        PERMISSIONS.SNACK_UPDATE,
+        PERMISSIONS.SNACK_DELETE,
+      ],
+    },
+    {
+      name: ROLE_DUMMY.ADMIN_USER,
+      permissions: [
+        PERMISSIONS.DASHBOARD,
+        PERMISSIONS.USER_READ,
+        PERMISSIONS.USER_DETAIL,
+        PERMISSIONS.USER_CREATE,
+        PERMISSIONS.USER_UPDATE,
+        PERMISSIONS.USER_DELETE,
+      ],
+    },
+    {
+      name: ROLE_DUMMY.ADMIN_ROLE,
+      permissions: [
+        PERMISSIONS.DASHBOARD,
+        PERMISSIONS.ROLE_READ,
+        PERMISSIONS.ROLE_DETAIL,
+        PERMISSIONS.ROLE_CREATE,
+        PERMISSIONS.ROLE_UPDATE,
+        PERMISSIONS.ROLE_DELETE,
+      ],
+    },
+  ];
 
-    try {
-        await db.insert(roles).values(dummyRoles).execute();
-        console.log("Seeding roles done!");
-    } catch (error) {
-        console.log("Seeding roles failed!", error);
-    }
-}
+  try {
+    await db.insert(roles).values(dummyRoles).execute();
+    console.log("Seeding roles done!");
+  } catch (error) {
+    console.log("Seeding roles failed!", error);
+  }
+};
--- libs/drizzle/seeders/dummy-user.ts
+++ libs/drizzle/seeders/dummy-user.ts
@@ -1,47 +1,52 @@
 import { hashPassword } from "../../auth/password";
 import { defaultImage, roles, users } from "../schema";
-import { PostgresJsDatabase } from "drizzle-orm/postgres-js";
+import { NodePgDatabase } from "drizzle-orm/node-postgres";
 import { ROLE_DUMMY } from "@/common/enums/role-dummy.enum";
 import { User } from "../schemas/user.schema";
 
-export const seedUsers = async (db: PostgresJsDatabase<Record<string, never>>) => {
-    console.log("Seeding users...");
-    const dummyUsers: User[] = [];
-    
-    const existingRoles = await db.select().from(roles);
+export const seedUsers = async (db: NodePgDatabase<Record<string, never>>) => {
+  console.log("Seeding users...");
+  const dummyUsers: User[] = [];
 
-    // Create 1000 dummy users
-    for (let i = 0; i < 1000; i++) {
-        const dummyUser: User = {
-            otp: null,
-            fullname: `John Doe ${i+1}`,
-            email: `johndoe@${i+1}email.com`,
-            address: `Address ${i+1}`,
-            password: await hashPassword(`password${i+1}`),
-            image: defaultImage,
-            emailVerified: new Date(),
-        };
+  const existingRoles = await db.select().from(roles);
 
-        // Define the user's role with conditions
-        if (i < 200) { // 0 - 199
-            dummyUser.roleId = existingRoles.find(role => role.name === ROLE_DUMMY.ADMIN)?.id;
-        } else if (i >= 200 && i < 400) { // 200 - 399
-            dummyUser.roleId = existingRoles.find(role => role.name === ROLE_DUMMY.STAFF)?.id;
-        } else if (i >= 400 && i < 600) { // 400 - 599
-            dummyUser.roleId = existingRoles.find(role => role.name === ROLE_DUMMY.ADMIN_SNACK)?.id;
-        } else if (i >= 600 && i < 800) { // 600 - 799
-            dummyUser.roleId = existingRoles.find(role => role.name === ROLE_DUMMY.ADMIN_USER)?.id;
-        } else { // 800 - 999
-            dummyUser.roleId = existingRoles.find(role => role.name === ROLE_DUMMY.ADMIN_ROLE)?.id;
-        }
+  // Create 1000 dummy users
+  for (let i = 0; i < 1000; i++) {
+    const dummyUser: User = {
+      otp: null,
+      fullname: `John Doe ${i + 1}`,
+      email: `johndoe@${i + 1}email.com`,
+      address: `Address ${i + 1}`,
+      password: await hashPassword(`password${i + 1}`),
+      image: defaultImage,
+      emailVerified: new Date(),
+    };
 
-        dummyUsers.push(dummyUser);
+    // Define the user's role with conditions
+    if (i < 200) {
+      // 0 - 199
+      dummyUser.roleId = existingRoles.find((role) => role.name === ROLE_DUMMY.ADMIN)?.id;
+    } else if (i >= 200 && i < 400) {
+      // 200 - 399
+      dummyUser.roleId = existingRoles.find((role) => role.name === ROLE_DUMMY.STAFF)?.id;
+    } else if (i >= 400 && i < 600) {
+      // 400 - 599
+      dummyUser.roleId = existingRoles.find((role) => role.name === ROLE_DUMMY.ADMIN_SNACK)?.id;
+    } else if (i >= 600 && i < 800) {
+      // 600 - 799
+      dummyUser.roleId = existingRoles.find((role) => role.name === ROLE_DUMMY.ADMIN_USER)?.id;
+    } else {
+      // 800 - 999
+      dummyUser.roleId = existingRoles.find((role) => role.name === ROLE_DUMMY.ADMIN_ROLE)?.id;
     }
 
-    try {
-        await db.insert(users).values(dummyUsers).execute();
-        console.log("Seeding users done!");
-    } catch (error) {
-        console.log("Seeding users failed!", error);
-    }
-}
+    dummyUsers.push(dummyUser);
+  }
+
+  try {
+    await db.insert(users).values(dummyUsers).execute();
+    console.log("Seeding users done!");
+  } catch (error) {
+    console.log("Seeding users failed!", error);
+  }
+};
