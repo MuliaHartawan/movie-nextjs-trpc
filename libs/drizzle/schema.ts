f5eb3319db16871733c34eebc1dc87fefaba73c9
Daniel Prastha <danielprastha4@gmail.com>
2024-07-18 11:10:21 +0700

    fix: migration file

--- libs/drizzle/schema.ts
+++ libs/drizzle/schema.ts
@@ -1,12 +1,12 @@
 import { relations } from "drizzle-orm";
-import { timestamp, pgTable, text, uuid, integer } from "drizzle-orm/pg-core";
+import { timestamp, pgTable, text, uuid, integer, varchar } from "drizzle-orm/pg-core";
 
 export const defaultImage =
   "https://w7.pngwing.com/pngs/205/731/png-transparent-default-avatar-thumbnail.png";
 
 export const roles = pgTable("app_role", {
   id: uuid("id").defaultRandom().primaryKey(),
-  name: text("name").notNull(),
+  name: varchar("name").notNull(),
   permissions: text("permissions").notNull().array(),
   createdAt: timestamp("created_at", { mode: "date" }).defaultNow(),
   updatedAt: timestamp("updated_At", { mode: "date" }).defaultNow(),
@@ -14,13 +14,13 @@ export const roles = pgTable("app_role", {
 
 export const users = pgTable("app_user", {
   id: uuid("id").defaultRandom().primaryKey(),
-  otp: text("otp"),
-  email: text("email").notNull().unique(),
+  otp: varchar("otp"),
+  email: varchar("email").notNull().unique(),
   image: text("image").default(defaultImage),
   roleId: uuid("role_id").references(() => roles.id, { onDelete: "cascade" }),
   address: text("address"),
-  fullname: text("fullname"),
-  password: text("password"),
+  fullname: varchar("fullname"),
+  password: varchar("password"),
   emailVerifiedAt: timestamp("email_verified_at", { mode: "date" }),
   createdAt: timestamp("created_at", { mode: "date" }).defaultNow(),
   updatedAt: timestamp("updated_at", { mode: "date" }).defaultNow(),
@@ -42,7 +42,7 @@ const defaultExpiryDate = new Date(now.setDate(now.getDate() + 1));
 
 export const snacks = pgTable("snacks", {
   id: uuid("id").defaultRandom().primaryKey(),
-  name: text("name").notNull(),
+  name: varchar("name").notNull(),
   cost: integer("cost").notNull().default(0),
   expiryDate: timestamp("expiry_date", { mode: "date" }).default(defaultExpiryDate),
   createdAt: timestamp("created_at", { mode: "date" }).defaultNow(),
