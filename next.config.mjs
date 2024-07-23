5a0354f71e81a6f402d9de04736c07644904d546
dot <macairm1@dot.co.id>
2024-07-23 09:27:45 +0700

    fix: adjust change on next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,20 @@
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     outputFileTracingIncludes: { "/": ["./node_modules/argon2/prebuilds/linux-x64/*.musl.*"] },
-    transpilePackages: ["admiral"],
-
+  },
+  transpilePackages: ["admiral"],
+  webpack: (config, { isServer }) => {
+    config.resolve.alias["@"] = path.join(__dirname, "");
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
   },
 };
 
