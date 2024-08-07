2269459364dcccf838008789737a98df86d005f3
Daniel Prastha <danielprastha4@gmail.com>
2024-08-07 11:14:24 +0700

    fix: role action import

--- src/app/(authenticated)/dashboard/(role)/roles/form/page.tsx
+++ src/app/(authenticated)/dashboard/(role)/roles/form/page.tsx
@@ -1,11 +1,11 @@
 import { PageProps } from "@/types/app";
-import { getRoleAction } from "@/server/role/actions/get-role";
 import { DashboardCreateRolesModule } from "../../_components/form";
+import { getRoleAction } from "@/server/role/actions/role.action";
 
 const DashboardCreateRolesPage = async (props: PageProps) => {
   const roleId = props.searchParams.id?.toString() ?? "";
   const data = await getRoleAction(roleId);
-  return <DashboardCreateRolesModule data={data?.success?.data} roleId={roleId} />;
+  return <DashboardCreateRolesModule data={data} roleId={roleId} />;
 };
 
 export default DashboardCreateRolesPage;
