eba71d7b4bc91d55c9b4b9edc3e7df2527aa1212
Nemixe <rafel.ami@gmail.com>
2024-09-05 10:21:44 +0700

    fix: reload every filter change

--- src/app/_components/guard/index.tsx
+++ src/app/_components/guard/index.tsx
@@ -1,15 +1,19 @@
 import { checkPermission } from "@/utils/permission";
 import { useSession } from "next-auth/react";
-import { FC, Fragment, PropsWithChildren } from "react";
+import { useMemo } from "react";
 
-export const Guard: FC<
-  PropsWithChildren & { permissions: string[]; customCondition?: boolean }
-> = async ({ permissions, customCondition, children }) => {
+export const Guard: React.FC<
+  React.PropsWithChildren<{ permissions: string[]; customCondition?: boolean }>
+> = ({ children, permissions, customCondition }) => {
   const userPermissions = useSession()?.data?.user?.role?.permissions;
-  const condition = checkPermission({
-    userPermissions,
-    permissions,
-    customCondition,
-  });
-  return <Fragment>{condition ? children : null}</Fragment>;
+  const permitted = useMemo(
+    () =>
+      checkPermission({
+        userPermissions,
+        permissions,
+        customCondition,
+      }),
+    [userPermissions, permissions, customCondition],
+  );
+  return <>{permitted ? children : null}</>;
 };
