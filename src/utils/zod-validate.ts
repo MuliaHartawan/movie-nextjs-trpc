7ca8e39eb3eddd8a1e29586e17a90bbca5af2904
Daniel Prastha <danielprastha4@gmail.com>
2024-09-05 10:07:21 +0700

    feat map zod error

--- src/utils/zod-validate.ts
+++ src/utils/zod-validate.ts
@@ -1,5 +1,18 @@
-import { ZodType } from "zod";
+import { ZodError, ZodType } from "zod";
+import BadRequestException from "../errors/BadRequestException";
 
-export const validate = async <T>(schema: ZodType<T>, data: T): Promise<T> => {
-  return schema.parseAsync(data);
+export const validate = async <T>(schema: ZodType<T>, data: T): Promise<T | void> => {
+  return schema.parseAsync(data).catch((error) => {
+    if (error instanceof ZodError) {
+      const zodError = error.errors;
+      const errorMessage = zodError.at(0)?.message;
+      const mapZodErrorPath = zodError.map((err) => {
+        return err.path.toString();
+      });
+
+      throw new BadRequestException(errorMessage ?? "Invalid input data", [
+        { path: mapZodErrorPath, message: errorMessage ?? "Invalid input data" },
+      ]);
+    }
+  });
 };
