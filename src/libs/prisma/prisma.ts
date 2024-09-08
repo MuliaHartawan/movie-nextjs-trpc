a7d6a58d06ccfde7cd0b70c5c0089f1ac1eccb2d
Daniel Prastha <danielprastha4@gmail.com>
2024-09-08 21:33:40 +0700

    fix: prisma generate

--- src/libs/prisma/prisma.ts
+++ src/libs/prisma/prisma.ts
@@ -4,18 +4,12 @@
 // Learn more:
 // https://pris.ly/d/help/next-js-best-practices
 
-import { PrismaClient } from "@prisma/client";
-import { Pool } from "pg";
-import { PrismaPg } from "@prisma/adapter-pg";
 import { QueryEvent } from "./query-event";
-
-const connectionString = process.env.DATABASE_URL;
+import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
-  const pool = new Pool({ connectionString, min: 1, max: 20 });
-  const adapter = new PrismaPg(pool);
   return new PrismaClient({
-    adapter,
+    // adapter,
     log: [
       {
         emit: "event",
