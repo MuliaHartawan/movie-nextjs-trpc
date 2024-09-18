9327d6d99422300965c25b704034852b9f642e48
Nemixe <rafel.ami@gmail.com>
2024-09-18 09:55:03 +0700

    fix: handle error and success state

--- src/app/(public)/auth/login/page.tsx
+++ src/app/(public)/auth/login/page.tsx
@@ -1,10 +1,49 @@
-"use server";
-import { NextPage } from "next";
-import { ReactElement } from "react";
-import { LoginFormComponent } from "./_components";
+"use client";
 
-const AuthLoginPage: NextPage = async (): Promise<ReactElement> => {
-  return <LoginFormComponent />;
+import type { FormProps } from "antd";
+import { Button, Form, Input } from "antd";
+import { TLoginForm } from "./_entities/schema";
+import { useLoginByCredentialsMutation } from "./_hooks/use-login-by-credentials-mutation";
+
+const FormLogin: React.FC = () => {
+  const loginByCredentialMutation = useLoginByCredentialsMutation();
+
+  const onFinish: FormProps<TLoginForm>["onFinish"] = async (values) => {
+    loginByCredentialMutation.mutate(values);
+  };
+
+  return (
+    <Form
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      autoComplete="off"
+    >
+      <Form.Item<TLoginForm>
+        label="Email"
+        name="email"
+        rules={[{ required: true, message: "Please input your username!" }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item<TLoginForm>
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: "Please input your password!" }]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type={"primary"} loading={loginByCredentialMutation.isPending} htmlType="submit">
+          Login
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 };
 
-export default AuthLoginPage;
+export default FormLogin;
