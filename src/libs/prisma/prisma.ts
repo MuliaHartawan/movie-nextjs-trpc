9131ba428ae0c8b562f189b8f38ad7e77e843529
Ardi Imawan <ardiimawan@gmail.com>
2024-10-18 18:38:47 +0900

    implement prisma-softdelete

--- src/libs/prisma/prisma.ts
+++ src/libs/prisma/prisma.ts
@@ -7,6 +7,7 @@
 import { pagination } from "prisma-extension-pagination";
 import { QueryEvent } from "./query-event";
 import { PrismaClient } from "@prisma/client";
+import { createSoftDeleteExtension } from "prisma-extension-soft-delete";
 
 const prismaClientSingleton = () => {
   return new PrismaClient({
@@ -25,39 +26,34 @@ declare const globalThis: {
 
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-// Soft delete middleware
-prisma.$use(async (params, next) => {
-  // Check incoming query type
-  if (params.model === "User")
-    if (params.action === "delete") {
-      // Delete queries
-      // Change action to an update
-      params.action = "update";
-      params.args.data = { deletedAt: new Date() };
-    }
-
-  if (params.action === "deleteMany") {
-    // Delete many queries
-    // Change action to an update
-    params.action = "updateMany";
-    params.args.data = { deletedAt: new Date() };
-  }
-
-  return next(params);
-});
-
 prisma.$on("query", (e: QueryEvent) => {
   console.log("Query: " + e.query);
   console.log("Params: " + e.params);
   console.log("Duration: " + e.duration + "ms");
 });
 
-export default prisma.$extends(
-  pagination({
-    pages: {
-      includePageCount: true,
-    },
-  }),
-);
+export default prisma
+  .$extends(
+    pagination({
+      pages: {
+        includePageCount: true,
+      },
+    }),
+  )
+  .$extends(
+    createSoftDeleteExtension({
+      models: {
+        User: true,
+      },
+      defaultConfig: {
+        field: "deletedAt",
+        createValue: (deleted) => {
+          console.log("deleted", deleted);
+          if (deleted) return new Date();
+          return null;
+        },
+      },
+    }),
+  );
 
 if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
