280ad2a5fe694dd4b80e7229f401678c1ec50d4b
Nemixe <rafel.ami@gmail.com>
2024-10-22 23:01:10 +0700

    fix: form error handling

--- src/server/trpc/router.ts
+++ src/server/trpc/router.ts
@@ -1,9 +1,23 @@
 import { initTRPC } from "@trpc/server";
 import superjson from "superjson";
 import { Context } from "./context";
+import { ZodError } from "zod";
 
 const t = initTRPC.context<Context>().create({
   transformer: superjson,
+  errorFormatter(opts) {
+    const { shape, error } = opts;
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zodError:
+          error.code === "BAD_REQUEST" && error.cause instanceof ZodError
+            ? error.cause.flatten()
+            : null,
+      },
+    };
+  },
 });
 
 export const router = t.router;
